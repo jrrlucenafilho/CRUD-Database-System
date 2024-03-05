5679f57d31494ddff6bd1266df04af93ad7fd0cc
Arthur Henrique <72824284+ArthurH35@users.noreply.github.com>
2024-03-05 15:57:21 -0300

    Menu added

--- src/main.py
+++ src/main.py
@@ -1,2 +1,37 @@
-import psycopg2 as pg2
-import tkinter as tk
+from crud import CRUD
+
+def menu():
+    print("\n===== Menu =====")
+    print("1. Cadastrar Produto")
+    print("2. Listar Produtos")
+    print("3. Buscar Produto por Nome")
+    print("4. Atualizar Produto")
+    print("5. Deletar Produto")
+    print("6. Gerar Relatório de Estoque")
+    print("7. Sair")
+
+def main():
+    while True:
+        menu()
+        opcao = input("Digite a opção desejada: ")
+        if opcao == "1":
+            CRUD.criar_produto()
+        elif opcao == "2":
+            CRUD.listar_produtos()
+        elif opcao == "3":
+            nome = input("Digite o nome do produto que deseja buscar: ")
+            CRUD.buscar_por_nome(nome)
+        elif opcao == "4":
+            CRUD.atualizar_produto()
+        elif opcao == "5":
+            CRUD.deletar_produto()
+        elif opcao == "6":
+            CRUD.gerar_relatorio_estoque()
+        elif opcao == "7":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
+if __name__ == "__main__":
+    main()
