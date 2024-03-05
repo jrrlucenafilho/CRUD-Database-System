c0b2f98516f658fdc154d0508802db9eeffe5aee
Arthur Henrique <72824284+ArthurH35@users.noreply.github.com>
2024-03-05 15:56:22 -0300

    CRUD mannager

---
+++ src/crud.py
@@ -0,0 +1,26 @@
+from produto import Produto
+
+class CRUD:
+    @staticmethod
+    def criar_produto():
+        Produto.criar_produto()
+
+    @staticmethod
+    def listar_produtos():
+        Produto.listar_produtos()
+
+    @staticmethod
+    def atualizar_produto():
+        Produto.atualizar_produto()
+
+    @staticmethod
+    def deletar_produto():
+        Produto.deletar_produto()
+
+    @staticmethod
+    def buscar_por_nome(nome):
+        Produto.buscar_por_nome(nome)
+
+    @staticmethod
+    def gerar_relatorio_estoque():
+        Produto.gerar_relatorio_estoque()
